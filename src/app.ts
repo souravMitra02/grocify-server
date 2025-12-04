0af9dfa09c5ac837f1371a7a7f316ac2223ea173
souravMitra02 <inf.souravmitra@gmail.com>
2025-12-04 13:00:57 +0600

    error fixed

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.route")
-
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://grocify-srote-new.netlify.app/",
+  origin: "https://grocify-srote-new.netlify.app",
   credentials: true
 }));
 
 app.use("/api/auth", authRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
