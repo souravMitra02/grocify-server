31cc3dd8b178fb76b3f8a1431078c950e0296c69
souravMitra02 <inf.souravmitra@gmail.com>
2025-12-04 13:14:50 +0600

    deploy issue fixed

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "https://grocify-srote-new.netlify.app",
+  origin: "https://grocify-client.vercel.app",
   credentials: true
 }));
 
